ab53b7602589f011cbe22dec202165fcfb215ae0
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-22 14:04:48 -0700

    implemented register for users

--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -1,5 +1,6 @@
 const db = require("../db");
-const { UnauthorizedError } = require("../utils/errors")
+const bcrypt = require("bcrypt");
+const { UnauthorizedError, BadRequestError } = require("../utils/errors")
 
 class User {
 
@@ -14,21 +15,70 @@ class User {
 
         // if any of this goes wrong, thrown an error
 
-        throw new UnauthorizedError("Invalis email/password combo")
+        throw new UnauthorizedError("Invalid email/password combo")
     }
 
     static async register(credentials) {
-        //user should submit their email, pw, date, and location
+        // user should submit their "email", "password", "firstName", "lastName", "location", "date"
         // if any fields are missing, throw an error
+        const requiredFields = ["email", "password", "firstName", "lastName", "location", "date"];
+        requiredFields.forEach(field => {
+            if(!credentials.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`);
+            }
+        });
 
-        //make sure no user already exists in the system with that email
+        if (credentials.email.indexOf("@") <= 0){
+            throw new BadRequestError(`Invalid email.`);
+        }
+
+        // make sure no user already exists in the system with that email
         // if yes, throw error
+        const existingUser = await User.fetchUserByEmail(credentials.email);
+        if (existingUser) {
+            throw new BadRequestError(`Duplicate email: ${credentials.email}`);
+        }
 
         // take uers password and hash it
+        const hashedPw = await bcrypt.hash(credentials.password, 12);
         // take users email and lowercase it
+        const lowercaseEmail = credentials.email.toLowerCase();
 
         // create a new user in the db with all their info
+        const results = await db.query(`
+			INSERT INTO users (
+                email, 
+                password, 
+                first_name, 
+                last_name, 
+                location, 
+                date) 
+            VALUES ($1, $2, $3, $4, $5, $6)
+			RETURNING email, password, first_name, last_name, location, date
+		`, 
+            [lowercaseEmail, hashedPw, credentials.firstName, credentials.lastName, credentials.location, credentials.date]
+		);
+
         // return user
+        const user = results.rows[0];
+		return user; 
+    }
+
+    //Fetch user by email
+    static async fetchUserByEmail(email) {
+
+        // if no email supplied
+        if(!email) {
+            throw new BadRequestError("No email provided");
+        }
+
+        // $1 is a query parameter
+        const query = `SELECT * FROM users WHERE email=$1`;
+        const result = await db.query(query, [email.toLowerCase()]);
+
+        const user = result.rows[0];
+
+        return user;
     }
 }
 
--- vaccine-hub-api/vaccine-hub-schema.sql
+++ vaccine-hub-api/vaccine-hub-schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL CHECK (POSITION('@' IN email) > 1),
     password TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL CHECK (POSITION('@' IN email) > 1),
     location TEXT NOT NULL,
     date TEXT NOT NULL
 );
