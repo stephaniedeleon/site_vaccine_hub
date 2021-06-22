b7ed2a8bbaf774aed30798f00a9c0b3f62432933
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-22 14:05:24 -0700

    update for login and register

--- vaccine-hub-api/routes/auth.js
+++ vaccine-hub-api/routes/auth.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
 const User = require("../models/user");
 const { NotFoundError } = require("../utils/errors");
 
 router.post("/login", async (req, res, next) => {
     try {
+        
         //take users email and password and attempting to authenticate them
+        const user = await User.login(req.body);
+        return res.status(200).json({ user });
+
     } catch (err) {
         next(err)
     }  
@@ -14,8 +17,12 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/register", async (req, res, next) => {
     try {
+
         // take users email and password (date and location of clinic?)
         // and create a new user in our database
+        const user = await User.register(req.body);
+        return res.status(201).json({ user });
+
     } catch (err) {
         next(err)
     }  
