ed6f4fc7c7d635ba692729151c7b648406f9e267
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-22 10:18:11 -0700

    added routes and model outline

---
+++ vaccine-hub-api/models/user.js
@@ -0,0 +1,32 @@
+const { UnauthorizedError } = require("../utils/errors")
+
+class User {
+
+    static async login(credentials) {
+        //user should submit their email and password
+        //if any of these fields are missing, throw an error
+
+        // lookup the user in db by email
+        // if a user id found compare the submitted password
+        // with password in db
+        // if there is a match, return the user
+
+        // if any of this goes wrong, thrown an error
+    }
+
+    static async register(credentials) {
+        //user should submit their email, pw, date, and location
+        // if any fields are missing, throw an error
+
+        //make sure no user already exists in the system with that email
+        // if yes, throw error
+
+        // take uers password and hash it
+        // take users email and lowercase it
+
+        // create a new user in the db with all their info
+        // return user
+    }
+}
+
+module.exports = User;
---
+++ vaccine-hub-api/routes/auth.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user");
+const { NotFoundError } = require("../utils/errors");
+
+router.post("/login", async (req, res, next) => {
+    try {
+        //take users email and password and attempting to authenticate them
+    } catch (err) {
+        next(err)
+    }  
+})
+
+router.post("/register", async (req, res, next) => {
+    try {
+        // take users email and password (date and location of clinic?)
+        // and create a new user in our database
+    } catch (err) {
+        next(err)
+    }  
+})
+
+module.exports = router;
